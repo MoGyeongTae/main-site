9baf7e70c7dffc32389dd16b4a2711fc9cd91596
Misty <misty@cemware.com>
2021-07-29 22:49:22 +0900

    refactor : main js -> tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  
+`;
+
+const Main : React.FC = () => {
+  return (
+    <Container>
+      Hi!
+    </Container>
+  )
+}
+
+export default Main;
